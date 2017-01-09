16d6c8048c8402184abcdb3e055cff9a58ec50f4
Lejugo <Lejugo123@gmail.com>
2017-01-09 03:18:51 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+        <form action = 'login.php' method='POST'>
+        Logowanie<br>
+        Username <input type='text' name='username'><br>
+        Password <input type='password' name='password'><br>
+        <input type='submit' value='log in'><br>
+        Rejestracja<br>
+        Username <input type='text' name='usernamer'><br>
+        Password <input type='password' name='passwordr'><br>
+        <input type='submit' value='rejestruj'><br>
+        </form>
+</html>
